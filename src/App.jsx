efe226dcad265a72c83eae6bf5e047e61b35a3bd
osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2024-05-02 15:23:23 +0100

    react: used mean features like usestate and also added a react-icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
 import React from "react";
-
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-
- import HomeCards from "./components/HomeCards";
-
-//  working 
-
-
+import HomeCards from "./components/HomeCards";
+//  working
 import Job from "./components/Job";
 
 import Views from "./components/Views";
 
-
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<h1>this app</h1>} />)
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-
-
-      {/*  */}
-      <Job />
-      <Views />
-    </>
-  );
+  return <RouterProvider router={router} />
 };
 
 export default App;
